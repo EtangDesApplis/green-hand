2433d25ac1f20f0f345b88ceed719ad0f7233266
AntoineNGUYEN31 <nguyen.ensma@gmail.com>
2020-11-28 19:16:41 +0100

    merge with api

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,16 @@
 # daemons
 
+```
 docker build -t etangdesapplis/gh-daemons daemons/
+```
+
+# api
+
+To build image:
+```
+docker build -t etangdesapplis/gh-daemons api/
+```
+To test:
+```
+curl https://chefphan.com/gh-api/ -d '{"email":"nguyen.ensma5@gmail.com","name":"Quan","info":"","seeds":[{"variety":"rose","seedingOutdoor":["3"],"seedingIndoor":["4"],"harvest":["6"],"exposition":"","timeToHarvest":"50"}]}' -H 'Content-Type: application/json'
+```
---
+++ backend/api/main.py
@@ -0,0 +1,126 @@
+from flask import Flask, request
+from flask_cors import CORS
+import os
+#from flask import send_from_directory
+#from pymongo import MongoClient
+import pprint
+from flask_pymongo import PyMongo
+from secrets import token_hex
+from time import time
+from datetime import datetime
+
+def getTimeStamp():
+  return datetime.now().strftime("[%Y-%m-%d][%H:%M:%S]")
+
+def printERROR(data):
+  print(getTimeStamp()+"[ERROR] "+data)
+
+def printINFO(data):
+  print(getTimeStamp()+"[INFO] "+data)
+
+def printWARN(data):
+  print(getTimeStamp()+"[WARN] "+data)
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://%s/green-hand"%(os.getenv("DB_SERVICE"))
+CORS(app)
+#/gh-api/{userID}-{countertoken} GET request to verify user
+mongo = PyMongo(app)
+
+
+@app.route('/<path:tokenInfo>')
+def verify(tokenInfo):
+  #ID prendre la premiere valeur avant le trait
+  id = int(tokenInfo.split('-')[0])
+  #print('id',id)
+  countertoken = tokenInfo.split('-')[1]
+  #print('countertoken', countertoken)
+  #IDuser
+  user=mongo.db.users.find_one({"id":id})
+  #pprint.pprint(user)
+  #print(user["counter-token"])
+  try:
+    if countertoken == user["counter-token"]:
+      mongo.db.users.update_one(
+                {"id": id},
+                {"$set": {"status":"verified"}}
+            )
+      printINFO("verified user with id = %d"%(id))
+      return {"Status":"OK"}
+    else :
+      printERROR("failed to verify user with id = %d"%(id))
+      return {"Status":"KO"}
+  except:
+    printWARN("unidentified request")
+    return {"Status":"KO"}
+
+
+
+@app.route('/', methods=['POST'])
+def post_route():
+  try:
+
+    # check if user is registered
+    if mongo.db.users.find_one({"email":data["email"]})==None:
+      # unknown user
+      # get max id
+      status=mongo.db.status.find_one()
+      if status==None:
+        # first run ever
+        uid=0
+        sid=0
+        uNb=0
+        sNb=0
+        #create status
+        mongo.db.status.insert_one({"uMID":-1,"uNb":0,"sMID":-1,"sNb":0})
+      else:
+        uid=status["uMID"]+1
+        sid=status["sMID"]+1
+        uNb=status["uNb"]
+        sNb=status["sNb"]
+
+      # add seed info here
+      seedList=[]
+      for item in data["seeds"]:
+        seed={
+                "id": sid,
+                "seedingOutdoor":item["seedingOutdoor"],
+                "seedingIndoor":item["seedingIndoor"],
+                "variety": item["variety"],
+                "harvest": item["harvest"],
+                "exposition": item["exposition"],
+                "timeToHarvest": item["timeToHarvest"]
+            }
+        mongo.db.users.insert_one(seed)
+        seedList.append(sid)
+        sid=sid+1
+
+      # add user
+      user={
+            "name": data["name"],
+            "id": uid,
+            "email":data["email"],
+            "status":"unverified",
+            "info": data["info"]
+            "seeds":seedList,
+            "token": token_hex(4),
+            "counter-token": token_hex(12),
+            "timeStamp": time()
+            }
+      mongo.db.users.insert_one(user)
+
+      #update status
+      mongo.db.status.update_one({},{"$set":{"uMID":uid,"uNb":uNb+1,"sMID":sid,"sNb":sNb+len(seedList)}})
+      printINFO("added new user with id = %d"%(uid))
+      return {"Status":"OK"}
+    else:
+      # reject for the moment due to lack of auth
+      printWARN("registried user failed to update with email = %s"%(data["email"]))
+      return {"Status":"email already used"}
+  except:
+    printERROR("bad request")
+    return {"Status":"KO"}
+
+if __name__=="__main__":
+  
+  app.run(host='0.0.0.0')
